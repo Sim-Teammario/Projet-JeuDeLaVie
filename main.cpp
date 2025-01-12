056c20a79b46d0300cab7b384f54bb3ff0c141b8
Sim-Teammario <simon.taveirne@proton.me>
2025-01-12 20:02:50 +0100

    Configuration de SFML terminée

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// Projet-JeuDeLaVie.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+//
+
+#include <iostream>
+
+#include "SFML/Graphics.hpp"
+#include "SFML/Window.hpp"
+#include "SFML/System.hpp"
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
